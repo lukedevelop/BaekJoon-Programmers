a84977bceada58b9208afe7c6cfaf7daf8985ef5
lukedevelop <102365004+lukedevelop@users.noreply.github.com>
2023-11-29 23:25:16 +0900

    [Bronze V] Title: N 찍기, Time: 1112 ms, Memory: 33744 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        for (int j = 1; j <= N; j++) {
+            System.out.println(j);
+        }
+
+    }
+}
