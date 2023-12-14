b91e881eb952ad19ddfc644cb20c1fe481a166c1
lukedevelop <102365004+lukedevelop@users.noreply.github.com>
2023-12-14 22:38:53 +0900

    [Bronze IV] Title: Do Not Touch Anything, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/13136. Do Not Touch Anything/Do Not Touch Anything.java
+++ 백준/Bronze/13136. Do Not Touch Anything/Do Not Touch Anything.java
@@ -1,22 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double R = Double.parseDouble(st.nextToken());
+		double C = Double.parseDouble(st.nextToken());
+		double N = Double.parseDouble(st.nextToken());
+		
 
-        Scanner sc = new Scanner(System.in);
-        int r = sc.nextInt(), c = sc.nextInt(), n = sc.nextInt();
-        
-        long x, y;
-        
-        if (r%n > 0)
-            x = r/n + 1;
-        else
-            x = r/n;
-        if (c%n > 0)
-            y = c/n + 1;
-        else
-            y = c/n;
-        System.out.println(x*y);
+		long RCCTV = (long) Math.ceil(R / N);
+		long CCCTV = (long) Math.ceil(C / N);
+		
 
-    }
+		long CCTV = RCCTV * CCCTV;
+		
+		System.out.println(CCTV);
+	}
+
 }
