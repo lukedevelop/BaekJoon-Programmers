0e52b4f008638d79fa9b590ed89d5c0f0d142a09
lukedevelop <102365004+lukedevelop@users.noreply.github.com>
2023-12-20 21:14:46 +0900

    [Bronze III] Title: 곱셈, Time: 216 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        String num2 = sc.next();
+
+        sc.close();
+
+        System.out.println(num1 * (num2.charAt(2) - '0'));
+        System.out.println(num1 * (num2.charAt(1) - '0'));
+        System.out.println(num1 * (num2.charAt(0) - '0'));
+        System.out.println(num1 * Integer.parseInt(num2));
+
+    }
+}
