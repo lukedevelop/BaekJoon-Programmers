3adbf9ec239147886fa096c09af6897a9bbf6226
lukedevelop <102365004+lukedevelop@users.noreply.github.com>
2024-02-14 13:15:08 +0900

    [Bronze III] Title: 세 수, Time: 204 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+        sc.close();
+
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+
+
+    }
+
+}
