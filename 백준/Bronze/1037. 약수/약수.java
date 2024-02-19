7d8ad44e59c2b20d3aca1756a36051583b38e75f
lukedevelop <102365004+lukedevelop@users.noreply.github.com>
2024-02-19 18:42:10 +0900

    [Bronze I] Title: 약수, Time: 216 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+
+
+
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int max = arr[num - 1];
+        int min = arr[0];
+
+        System.out.println(max * min);
+    }
+
+}
