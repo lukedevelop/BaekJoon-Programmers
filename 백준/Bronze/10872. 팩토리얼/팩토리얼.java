f0cac580062d375264343be151104413a8538daf
lukekim <102365004+lukedevelop@users.noreply.github.com>
2024-03-11 16:49:25 +0900

    [Bronze V] Title: 팩토리얼, Time: 204 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println(factorial(num));
+
+    }
+
+    static int factorial(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+}
