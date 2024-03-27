d50717026e9eae54a2744361008cb5713109370b
lukekim <102365004+lukedevelop@users.noreply.github.com>
2024-03-27 12:14:51 +0900

    [Bronze I] Title: 약수들의 합, Time: 236 ms, Memory: 19328 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -3,29 +3,30 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		while(true) {
-			int n = scan.nextInt();
-			if(n == -1)
+			int N = sc.nextInt();
+			if(N == -1){
 				break;
+            }
 			
-			int[] arr = new int[n]; 
-			int sum = 0;			
+			int[] arr = new int[N]; 
+			int res = 0;			
 			int index = 0;			
-			for(int i=1; i<n; i++) {
-				if(n%i == 0) {		
+			for(int i=1; i<N; i++) {
+				if(N%i == 0) {		
 					arr[index++] = i;	
-					sum += i;			
+					res += i;
 				}
 			}
 			
-			if(sum != n) {
-				System.out.println(n + " is NOT perfect.");
+			if(res != N) {
+				System.out.println(N + " is NOT perfect.");
 				continue;
 			}
 			
-			System.out.print(n + " = ");
+			System.out.print(N + " = ");
 			for(int i=0; i<index; i++) {
 				if(i == index-1)
 					System.out.print(arr[i]);
@@ -35,7 +36,7 @@ public class Main {
 			System.out.println();
 		}
 		
-		scan.close();
+		sc.close();
 	}
 
 }
