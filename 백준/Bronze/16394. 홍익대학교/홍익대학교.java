1c6c0bfdd4bdc33502b8e9ebcfd8b13f0feb4915
lukekim <102365004+lukedevelop@users.noreply.github.com>
2024-04-13 10:42:53 +0900

    [Bronze V] Title: 홍익대학교, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = 1946;
+        int m = sc.nextInt();
+
+        System.out.println(m - n);
+
+
+    }
+}
