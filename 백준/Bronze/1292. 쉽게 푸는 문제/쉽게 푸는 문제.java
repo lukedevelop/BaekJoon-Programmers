f6980cb087da153ecb32c03a98772779d9a264a4
lukekim <102365004+lukedevelop@users.noreply.github.com>
2024-05-06 16:50:52 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 244 ms, Memory: 31804 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j <= i; j++) {
+                list.add(i + 1);
+            }
+        }
+
+        int sum = 0;
+        for (int i = a - 1 ; i <= b - 1; i++) {
+            sum += list.get(i);
+        }
+        System.out.println(sum);
+
+    }
+
+}
