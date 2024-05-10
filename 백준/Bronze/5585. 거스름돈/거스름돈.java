d3e775dec730d7174c33c4da6372337331f2c2b7
lukekim <102365004+lukedevelop@users.noreply.github.com>
2024-05-10 15:11:07 +0900

    [Bronze II] Title: 거스름돈, Time: 220 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int money = 1000;
+        int m = sc.nextInt();
+
+
+        int tot = money - m;
+        int change = 0;
+
+        while (tot > 0){
+
+
+            if (tot >= 500) {
+                tot -= 500;
+                change++;
+            }
+            if (tot >= 100 && tot < 500) {
+                tot -= 100;
+                change++;
+            }
+            if (tot >= 50 && tot < 100) {
+                tot -= 50;
+                change++;
+            }
+            if (tot >= 10 && tot < 50) {
+                tot -= 10;
+                change++;
+            }
+            if (tot >= 5 && tot < 10) {
+                tot -= 5;
+                change++;
+            }
+            if (tot >= 1 && tot < 5){
+                tot -= 1;
+                change++;
+            }
+
+            if (tot == 0) {
+                System.out.println(change);
+                break;
+            }
+
+        }
+
+
+    }
+
+}
