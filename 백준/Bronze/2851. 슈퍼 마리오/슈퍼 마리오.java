2485797bf1cbca40c4f3b79717e85229517e4931
lukekim <102365004+lukedevelop@users.noreply.github.com>
2024-05-11 14:55:05 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int sc = 100;
+        int answer = 0;
+
+        for (int i = 0; i < 10; i++) {
+            sum += Integer.parseInt(br.readLine());
+            if (Math.abs(100 - sum) <= sc) {
+                sc = Math.abs(100 - sum);
+                answer = sum;
+            }
+        }
+        System.out.println(answer);
+    }
+
+}
