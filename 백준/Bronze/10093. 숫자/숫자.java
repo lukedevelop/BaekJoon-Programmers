ab95038df9047ac558c133c265543fe3e641669e
lukekim <102365004+lukedevelop@users.noreply.github.com>
2024-05-16 15:11:05 +0900

    [Bronze II] Title: 숫자, Time: 256 ms, Memory: 23916 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.java
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -1,29 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        long a = sc.nextInt();
-        long b = sc.nextInt();
-
-        if (a < b) {
-            System.out.println(b - a - 1);
-            for (long i = a+1; i < b; i++) {
-                System.out.print(i + " ");
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());;
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        if(a>b){
+            long tmp = a;
+            a = b;
+            b = tmp;
         }
-        if (a > b) {
-            System.out.println(a - b - 1);
-            for (long i = b+1; i < a; i++) {
-                System.out.print(i + " ");
-            }
+        if(a==b)
+            sb.append(b-a).append("\n");
+        else sb.append(b-a-1).append("\n");
+        for(long i=a+1;i<b;i++){
+            sb.append(i).append(" ");
         }
-        if (a == b) {
-            System.out.println(0);
-        }
-
+        System.out.println(sb);
     }
-
-
 }
