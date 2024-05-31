5256e2f4ac80d21c89df606e1612a6de2ca9e104
lukekim <102365004+lukedevelop@users.noreply.github.com>
2024-05-31 17:19:23 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 128 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.java
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.java
@@ -1,19 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] split = s.split("");
 
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        String[] split = str.split("");
-
+        int chk = 1;
         for (int i = 0; i < split.length; i++) {
             System.out.print(split[i]);
-            if (i % 10 == 9) {
+            if (i == 10 * chk - 1) {
+                chk++;
                 System.out.println();
             }
         }
-
-
     }
 }
