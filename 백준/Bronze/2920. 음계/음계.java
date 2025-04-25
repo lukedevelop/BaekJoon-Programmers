9837810bba1f20a93a8ac8e8cc2dfcd882141c30
lukekim <102365004+lukedevelop@users.noreply.github.com>
2025-04-25 16:42:10 +0900

    [Bronze II] Title: 음계, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -1,15 +1,18 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] arr = new int[8];
 		
+		
 		for(int i = 0; i < 8; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		boolean asc = true;
@@ -31,7 +34,6 @@ public class Main {
 			System.out.println("mixed");
 		}
 		
-		sc.close();
 		
 	}
 
