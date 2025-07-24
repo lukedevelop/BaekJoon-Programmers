1e37bd7dc7f245ef0ea33233b62ca2cc1d961d57
lukekim <102365004+lukedevelop@users.noreply.github.com>
2025-07-24 22:44:43 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 196 ms, Memory: 18352 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {1,1,2,2,2,8};
+        int[] res = new int [arr.length];
+
+        for(int i = 0; i < arr.length; i++){
+            int[] chk = new int[arr.length];
+            chk[i] = sc.nextInt();
+            res[i] = arr[i] - chk[i];
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(res[i]+" ");
+        }
+    }
+
+}
