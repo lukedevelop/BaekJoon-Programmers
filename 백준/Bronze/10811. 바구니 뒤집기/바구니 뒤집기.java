e65df949b14b6b3f9a7e9b84106ca4c12c097b1e
lukekim <102365004+lukedevelop@users.noreply.github.com>
2025-08-03 14:49:44 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 244 ms, Memory: 18512 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int temp = 0;
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = i+1;
+        }
+
+        for(int k = 0; k < m; k++){
+            int i = sc.nextInt() - 1;
+            int j = sc.nextInt() - 1;
+
+            while(i < j){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+}
