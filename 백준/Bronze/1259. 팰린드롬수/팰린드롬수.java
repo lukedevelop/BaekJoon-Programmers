66f3e5511ebb19fc3b0164a90308575993731fdb
lukekim <102365004+lukedevelop@users.noreply.github.com>
2025-10-01 15:18:28 +0900

    [Bronze I] Title: 팰린드롬수, Time: 172 ms, Memory: 17336 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        while(true){
+            String str = sc.nextLine();
+
+            if(str.equals("0")){
+                break;
+            }
+
+            String reverse = new StringBuilder(str).reverse().toString();
+
+            if(str.equals(reverse)){
+                System.out.println("yes");
+            } else{
+                System.out.println("no");
+            }
+
+            }
+
+        }
+}
