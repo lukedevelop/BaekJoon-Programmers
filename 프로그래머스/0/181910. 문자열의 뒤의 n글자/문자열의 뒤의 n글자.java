7caf50378d868dfbd95c2e3743fb739e0558e43c
lukekim <102365004+lukedevelop@users.noreply.github.com>
2025-10-09 20:59:25 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 23.77 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        
+        for(int i = my_string.length() - n; i < my_string.length(); i++){
+            char ch = my_string.charAt(i);
+            answer += ch;
+        }
+        
+        return answer;
+    }
+}
