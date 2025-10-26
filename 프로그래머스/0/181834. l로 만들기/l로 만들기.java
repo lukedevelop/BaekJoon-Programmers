6721ac58bdc4a8da9a1ed2575b027580ba1ead89
lukekim <102365004+lukedevelop@users.noreply.github.com>
2025-10-26 09:29:09 +0900

    [level 0] Title: l로 만들기, Time: 4.92 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        char[] chars = myString.toCharArray();
+        
+        for(int i = 0; i < chars.length; i++){
+            if(chars[i] >= 'a' && chars[i] <= 'k'){
+                chars[i] = 'l';
+            }
+        }
+        
+        String answer = new String(chars);
+        
+        return answer;
+    }
+}
