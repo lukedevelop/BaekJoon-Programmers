9db0f40d85a6a53851af4e8462de69768a800dd3
lukekim <102365004+lukedevelop@users.noreply.github.com>
2025-11-04 14:25:47 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        int n = Integer.parseInt(n_str);
+        String answer = Integer.toString(n);
+        
+        return answer;
+    }
+}
