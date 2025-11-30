bd6547610266b692002bfad2fd1de797acd75630
lukekim <102365004+lukedevelop@users.noreply.github.com>
2025-11-30 15:24:48 +0900

    [level 0] Title: A 강조하기, Time: 0.04 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        answer = myString.toLowerCase();
+        
+        if(answer.contains("a")){
+           answer = answer.replace("a","A");
+        }
+        
+        return answer;
+    }
+}
